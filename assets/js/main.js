bf0f8eeb2c29b88c41dc1acdbc9add6500014f14
hamzah-08 <hamzahsham@hmail.com>
2022-07-17 12:37:06 +0530

    Updated page

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".testimonialSwiper", {
+//   loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+  },
+});
